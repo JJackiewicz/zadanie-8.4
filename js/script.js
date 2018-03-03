17a4f52b9b652ab46826e8769c914544094b5b39
Jacek Jackiewicz <jjackiewicz86@gmail.com>
2018-03-03 15:08:30 +0100

    Add project files and comments

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// scripts.js
+
+var name = prompt('Enter your name'); // inline comment
+
+// linear comment 
+alert('Hello, ' + name);
+
+/**multi
+ * line
+ * comment
+ */
+console.log('Hello, ' + name);
